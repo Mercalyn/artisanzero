8ce08af2253c2ef65681d54d5767979fc1862ee1
Emily Thomas <alliancerune@gmail.com>
2017-08-13 15:52:32 -0600

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,11 @@
+<html>
+<link href="cascade.css" rel="stylesheet" type="text/css">
+<div border="0" class="tablcolo"><div border="1" class="nonet">
+<div class="sunet"><h1>Navigation</h1></div>
+<h4><a href="BLANKLINK">Who Are We?</a></h4>
+<h4><a href="BLANKLINK">Developer Profiles</a></h4>
+<h4><a href="BLANKLINK">Graphics</a></h4>
+<br><br>
+<h4><a href="policy.php">Privacy Policy</a></h4>
+<h4><a href="policy.php">Contact Us</a></h4>
+</div></div>
---
+++ titlebar.php
@@ -0,0 +1,24 @@
+<html>
+<link href="cascade.css" rel="stylesheet" type="text/css">
+
+
+<div class="banner">
+
+
+
+<table border="0" class="navin"><tr>
+	<th>
+		<a href="http://www.ArtisansRepublic.com/?en=us"><h2> home </h2></a>
+	</th><th>
+		<a href="http://www.studios.ArtisansRepublic.com/"><h2> studios </h2></a>
+	</th><th>
+		<a href="http://www.science.ArtisansRepublic.com"><h2> science </h2></a>
+	</th><th>
+		<a href="cu.php"><h2> contact us </h2></a>
+	</th>
+</tr>
+
+
+</div>
+</table>
+</html>
